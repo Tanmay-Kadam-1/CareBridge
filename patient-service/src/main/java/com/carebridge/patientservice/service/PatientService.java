4e953a6f45a0d81a2cbfc1bc2bbd9eb362d36265
Tanmay Kadam <justcoding1482@gmail.com>
2026-01-05 19:19:05 +0530

    Added Kafka event-driven architecture where the Patient Service produces PatientCreated events and the Analytics Service acts as a consumer to process analytics and recovery workflows.

--- patient-service/src/main/java/com/carebridge/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/carebridge/patientservice/service/PatientService.java
@@ -1,15 +1,14 @@
 package com.carebridge.patientservice.service;
 
-import billing.BillingResponse;
 import com.carebridge.patientservice.Exception.EmailAlreadyExistsException;
 import com.carebridge.patientservice.Exception.PatientNotFoundException;
 import com.carebridge.patientservice.dto.PatientRequestDTO;
 import com.carebridge.patientservice.dto.PatientResponseDTO;
 import com.carebridge.patientservice.grpc.BillingServiceGrpcClient;
+import com.carebridge.patientservice.kafka.KafkaProducer;
 import com.carebridge.patientservice.mapper.PatientMapper;
 import com.carebridge.patientservice.model.Patient;
 import com.carebridge.patientservice.repository.PatientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -23,10 +22,13 @@ public class PatientService {
 
     private final BillingServiceGrpcClient billingServiceGrpcClient;
 
-    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient)
+    private final KafkaProducer kafkaProducer;
+
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient, KafkaProducer kafkaProducer)
     {
         this.patientRepository = patientRepository;
         this.billingServiceGrpcClient = billingServiceGrpcClient;
+        this.kafkaProducer = kafkaProducer;
     }
 
     public List<PatientResponseDTO> getPatients()
@@ -43,9 +45,19 @@ public class PatientService {
     {
         if(patientRepository.existsByEmail(patientRequestDTO.getEmail())) throw new EmailAlreadyExistsException("This Email address is already registered for another Patient" + patientRequestDTO.getEmail());
 
-        Patient patientNew  = patientRepository.save( PatientMapper.toModel(patientRequestDTO) );
+//        Patient patientNew  = patientRepository.save( PatientMapper.toModel(patientRequestDTO) );
+          Patient patientNew = new Patient();
+
+          patientNew.setId(UUID.randomUUID());
+          patientNew.setName(patientRequestDTO.getName());
+          patientNew.setEmail(patientRequestDTO.getEmail());
+          patientNew.setAddress(patientRequestDTO.getAddress());
+          patientNew.setRegisteredDate(LocalDate.now());
+          patientNew.setDateOfBirth(LocalDate.now());
 
         billingServiceGrpcClient.createBillingAccount(patientNew.getId().toString(),patientNew.getName(),patientNew.getEmail());
+
+        kafkaProducer.sendEvent(patientNew);
 
         return PatientMapper.toDTO(patientNew);
     }
